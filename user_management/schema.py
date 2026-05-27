1dfa9adcf37f239ed64a5b9423a7e55b6d284264
dan <danial.hedaiat@gmail.com>
2026-05-27 13:25:21 +0330

    [add] - create schema for user management models

---
+++ user_management/schema.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class UserBaseSchema(BaseModel):
+    phone_number: int = Field(..., max_length=15)
+    first_name: int = Field(..., max_length=50)
+    last_name: int = Field(..., max_length=50)
+
+
+class UserCompleteSchema(UserBaseSchema):
+    id: int
+    is_verify: bool
+    create_data: datetime
+
+
+class UserSocialMediaBaseSchema(BaseModel):
+    social_media: str = Field(..., max_length=50)
+    user_id: int
+
+
+class UserSocialMediaResponseSchema(UserSocialMediaBaseSchema):
+    id: int
+
+
+class RoleBaseSchema(BaseModel):
+    name: str = Field(..., max_length=30)
+
+
+
+
+class RoleResponseSchema(RoleBaseSchema):
+    id: int
+
+
+class UserRoleBaseSchema(BaseModel):
+    user_id: int
+    role_id: int
