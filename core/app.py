58b7a3b490f23164a3797ab988e94869969d060f
dan <danial.hedaiat@gmail.com>
2026-05-27 09:57:13 +0330

    [add] - create app file

---
+++ core/app.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application startup")
+    yield
+    print("Application shutdown")
+
+app = FastAPI(
+    title="Kaargoosha Emam Reza Investment",
+    lifespan=lifespan,
+    version="1.0.0",
+    contact={
+        "name": "Danial Hadi",
+        "email": "danialhedaiat@gmail.com",
+        "phone_number": "+989308222060"
+    }
+)
