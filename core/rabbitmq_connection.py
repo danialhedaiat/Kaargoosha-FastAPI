092e540d7701060c48f7713480826ac1e4f7c18c
dan <danial.hedaiat@gmail.com>
2026-05-27 15:38:39 +0330

    [update] - change rabbitmq connection to singleton design

--- core/rabbitmq_connection.py
+++ core/rabbitmq_connection.py
@@ -3,11 +3,18 @@ import pika
 from core.settings import settings
 
 
-def get_rabbitmq_connection():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=settings.RABBITMQ_HOST,
-        port=settings.RABBITMQ_PORT,
-        credentials=pika.PlainCredentials(settings.RABBITMQ_USERNAME, settings.RABBITMQ_PASSWORD)
-    ))
-    channel = connection.channel()
-    return connection, channel
+class RabbitMQConnection:
+    instance = None
+    def __new(cls, *args, **kwargs):
+        if cls.instance:
+            return cls.instance
+        cls.instance = super(RabbitMQConnection, cls).__new__(cls, *args, **kwargs)
+
+    def __init__(self):
+        if not hasattr(self, 'connection'):
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host=settings.RABBITMQ_HOST,
+                port=settings.RABBITMQ_PORT,
+                credentials=pika.PlainCredentials(settings.RABBITMQ_USERNAME, settings.RABBITMQ_PASSWORD)
+            ))
+            self.channel = self.connection.channel()
