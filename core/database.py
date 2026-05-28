5180df59e47a4a78f4c7e30b780ba387a5785933
dan <danial.hedaiat@gmail.com>
2026-05-28 16:00:24 +0330

    [update] - change database url env

--- core/database.py
+++ core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from core.settings import settings
 
 engine = create_engine(
-    settings.DATABASE_URL,
+    settings.ALEMBIC_DATABASE_URL,
     connect_args={"check_same_thread": False},
 )
 
