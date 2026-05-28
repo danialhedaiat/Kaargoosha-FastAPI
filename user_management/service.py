071b0b73ecc3b9dfe9602f0129bd85e8a8ebf79a
dan <danial.hedaiat@gmail.com>
2026-05-28 23:47:48 +0330

    [update] - update init method and remove old db communication

--- user_management/service.py
+++ user_management/service.py
@@ -1,12 +1,8 @@
-from user_management.models import UserModel
-from user_management.repository  import UserRepository
 from core.database import SessionLocal, get_db
 
 
 class UserService:
 
-    def __init__(self, user_repo: UserRepository):
-        self.user_repo = user_repo
     def __init__(self):
         self.db: SessionLocal = get_db()
 
