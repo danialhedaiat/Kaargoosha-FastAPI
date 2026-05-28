ebb53ab889a946ff4e795ca4819d49332ba8a83a
dan <danial.hedaiat@gmail.com>
2026-05-28 23:48:38 +0330

    [update] - create check phone number exist method

--- user_management/service.py
+++ user_management/service.py
@@ -1,10 +1,17 @@
 from core.database import SessionLocal, get_db
+from user_management.models import UserModel, UserSocialMediaID
 
 
 class UserService:
 
     def __init__(self):
         self.db: SessionLocal = get_db()
+
+    def check_phone_number_exist(self, data):
+        exists = self.db.query(UserModel).filter_by(phone_number=data["phone_number"]).all()
+        if exists:
+            return {"message": "User already exists"}
+        return {"message": "User does not exist"}
 
 
     def delete(self, data):
