232fac07b1ddd9dfa2820897da006591962c744c
dan <danial.hedaiat@gmail.com>
2026-05-28 12:17:27 +0330

    [update] - create logger with readable formater

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -17,3 +18,47 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_PATH)
 
 settings = Settings()
+
+class CustomFormatter(logging.Formatter):
+
+    def format(self, record):
+        RESET = "\033[0m"
+        WHITE = "\033[97m"
+        LIGHT_BLUE = "\033[94m"
+        PINK = "\033[95m"
+        GREEN = "\033[92m"
+
+        asctime = self.formatTime(record, self.datefmt)
+
+        level_color = {
+            "INFO": LIGHT_BLUE,
+            "WARNING": "\033[93m",
+            "ERROR": "\033[91m",
+        }.get(record.levelname, WHITE)
+
+        msg = record.getMessage()
+
+        formatted = (
+            f"{GREEN}{asctime}{RESET} "
+            f"{level_color}{record.levelname}{RESET} "
+            f"{PINK}{record.name}{RESET} "
+            f"{WHITE}{msg}{RESET}"
+        )
+
+
+        return formatted
+
+def setup_logging():
+    handler = logging.StreamHandler()
+
+    handler.setFormatter(CustomFormatter())
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
+    return logger
+
+logger = setup_logging()
