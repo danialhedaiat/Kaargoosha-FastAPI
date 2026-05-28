d3d58a9e566b0748fc99ced58206de3a819c7987
dan <danial.hedaiat@gmail.com>
2026-05-28 12:14:47 +0330

    [update] - update models to make them type sensitive

--- user_management/models.py
+++ user_management/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from core.database import Base
 
@@ -9,12 +9,12 @@ from core.database import Base
 class UserModel(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    phone_number = Column(String(15), unique=True)
-    is_verify = Column(Boolean, default=False)
-    first_name = Column(String(50), nullable=False)
-    last_name = Column(String(50), nullable=False)
-    create_date = Column(DateTime, default=datetime.datetime.now())
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    phone_number: Mapped[str] = mapped_column(String(15), unique=True)
+    is_verify: Mapped[bool]= mapped_column(Boolean, default=False)
+    first_name: Mapped[str]= mapped_column(String(50), nullable=False)
+    last_name: Mapped[str]= mapped_column(String(50), nullable=False)
+    create_date: Mapped[datetime]= mapped_column(DateTime, default=datetime.datetime.now())
 
     user_social_medias = relationship(
         "UserSocialMediaID",
@@ -27,10 +27,10 @@ class UserModel(Base):
 class UserSocialMediaID(Base):
     __tablename__ = 'user_social_media_id'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    username = Column(String(100), nullable=False)
-    social_media = Column(String(50), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    username: Mapped[str] = mapped_column(String(100), nullable=False)
+    social_media: Mapped[str] = mapped_column(String(50), nullable=False)
 
     user = relationship("UserModel", back_populates="user_social_medias")
 
@@ -38,8 +38,8 @@ class UserSocialMediaID(Base):
 class Role(Base):
     __tablename__ = "roles"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(30), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
 
     user = relationship("UserRole", back_populates="role", cascade="all, delete, delete-orphan")
 
@@ -47,10 +47,10 @@ class Role(Base):
 class UserRole(Base):
     __tablename__ = "user_role"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    role_id = Column(Integer, ForeignKey('roles.id'), nullable=False)
+    user_id = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
+    role_id = mapped_column(Integer, ForeignKey('roles.id'), nullable=False)
 
     user = relationship("UserModel", back_populates="role")
     role = relationship("Role", back_populates="user")
