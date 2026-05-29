24022c055af129db1a4e0a84a672ff0f9c412d35
dan <danial.hedaiat@gmail.com>
2026-05-29 19:34:14 +0330

    [update] - create new unique constraint for user role model class

--- user_management/models.py
+++ user_management/models.py
@@ -51,6 +51,10 @@ class Role(Base):
 class UserRole(Base):
     __tablename__ = "user_role"
 
+    __table_args__ = (
+        UniqueConstraint("user_id", "role_id", name="user_role_unique"),
+    )
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
     user_id = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
