e7bd851c3dcbb424e43e1cc9ea17297d6e1c476e
dan <danial.hedaiat@gmail.com>
2026-05-29 19:45:00 +0330

    [update] - make role name unique

--- user_management/models.py
+++ user_management/models.py
@@ -42,6 +42,10 @@ class UserSocialMediaID(Base):
 class Role(Base):
     __tablename__ = "roles"
 
+    __table_args__ = (
+        UniqueConstraint("name", name="name_unique"),
+    )
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False)
 
