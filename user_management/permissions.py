da1c840cb14a7921c15361ce0ce29d41a02a08b3
dan <danial.hedaiat@gmail.com>
2026-05-29 23:51:18 +0330

    [add] - create hardcode permissions file

---
+++ user_management/permissions.py
@@ -0,0 +1,13 @@
+class Permissions:
+    # User
+    USER_CREATE  = "user.create"
+    USER_READ    = "user.read"
+    USER_UPDATE  = "user.update"
+    USER_DELETE  = "user.delete"
+
+    # Role
+    ROLE_CREATE  = "role.create"
+    ROLE_READ    = "role.read"
+    ROLE_UPDATE  = "role.update"
+    ROLE_DELETE  = "role.delete"
+    ROLE_ASSIGN  = "role.assign"
