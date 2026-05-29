542ed39bef730a1a79ea327c1bbc71a99858eef4
dan <danial.hedaiat@gmail.com>
2026-05-30 00:38:35 +0330

    [update] - add some new hardcode permissions

--- user_management/permissions.py
+++ user_management/permissions.py
@@ -11,3 +11,11 @@ class Permissions:
     ROLE_UPDATE  = "role.update"
     ROLE_DELETE  = "role.delete"
     ROLE_ASSIGN  = "role.assign"
+    ROLE_REVOKE  = "role.revoke"
+
+    #Permission
+    PERMISSION_CREATE  = "role.create"
+    PERMISSION_READ    = "role.read"
+    PERMISSION_UPDATE  = "role.update"
+    PERMISSION_DELETE  = "role.delete"
+    PERMISSION_ASSIGN  = "role.assign"
